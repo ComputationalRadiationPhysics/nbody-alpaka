07cfc6586affee19a2780347a196de5d215c6610
Vincent Ridder <vincent.ridder@mailbox.tu-dresden.de>
2015-12-22 17:13:22 +0100

    updatePosition Kernel and Test

---
+++ tests/updatePositionsKernel/CMakeFiles/alpaka.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vincent/Dokumente/alpaka/nbody-alpaka/alpaka/include/alpaka/core/Dummy.cpp" "/home/vincent/Dokumente/alpaka/nbody-alpaka/tests/updatePositionsKernel/CMakeFiles/alpaka.dir/home/vincent/Dokumente/alpaka/nbody-alpaka/alpaka/include/alpaka/core/Dummy.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ALPAKA_ACC_CPU_B_OMP2_T_SEQ_ENABLED"
+  "ALPAKA_ACC_CPU_B_SEQ_T_OMP2_ENABLED"
+  "ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED"
+  "ALPAKA_ACC_CPU_B_SEQ_T_THREADS_ENABLED"
+  "ALPAKA_DEBUG=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/vincent/Dokumente/alpaka/nbody-alpaka/alpaka/include"
+  "../../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
