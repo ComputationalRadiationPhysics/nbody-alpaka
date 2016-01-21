17e818d61370fd8856adabcc49d08d2e88c3512b
Valentin Gehrke <madmaurice@zom.bi>
2016-01-21 19:11:17 +0100

    Moving camera + shiny bodies

--- tests/simulationTest/vision.py
+++ tests/simulationTest/vision.py
@@ -12,14 +12,21 @@ points=datas.pop(0).split('|')
 points= [tuple(map(float,p.split(';'))) for p in points]
 v_points=[]
 for i in range(len(points)):
-        v_points.append(sphere(pos=points[i],radius= masses[i], color= color.red))
+        v_points.append(sphere(pos=points[i],radius= 3e-4 * (masses[i])**0.3, color= color.white, make_trail=True, material=materials.shiny, retain=400))
         print (points[i])
 
 while true :#updating position
     for data in datas:
-            rate(10)
+            rate(50)
             points=data.split('|')
             points=[tuple(map(float,p.split(';'))) for p in points]
+            newcenter = [ 0,0,0 ]
             for i in range(len(v_points)):
-                    v_points[i].pos= points[i]
+                for j in [0,1,2]:
+                    newcenter[j] += points[i][j]
+                v_points[i].pos= points[i]
+            for j in [0,1,2]:
+                newcenter[j] /= len(v_points)
+            
+            scene.center = tuple( newcenter )
 
