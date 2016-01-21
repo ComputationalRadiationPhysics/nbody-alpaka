d545c1c754cb67c143ecd3b2b04577f3ac8bdd83
Valentin Gehrke <madmaurice@zom.bi>
2016-01-21 15:00:08 +0100

    Fixed tabs

--- tests/simulationTest/vision.py
+++ tests/simulationTest/vision.py
@@ -12,14 +12,14 @@ points=datas.pop(0).split('|')
 points= [tuple(map(float,p.split(';'))) for p in points]
 v_points=[]
 for i in range(len(points)):
-	v_points.append(sphere(pos=points[i],radius= masses[i], color= color.red))
+        v_points.append(sphere(pos=points[i],radius= masses[i], color= color.red))
         print (points[i])
 
 while true :#updating position
     for data in datas:
-    	    rate(10)
+            rate(10)
             points=data.split('|')
-	    points=[tuple(map(float,p.split(';'))) for p in points]
-	    for i in range(len(v_points)):
-	    	    v_points[i].pos= points[i]
+            points=[tuple(map(float,p.split(';'))) for p in points]
+            for i in range(len(v_points)):
+                    v_points[i].pos= points[i]
 
