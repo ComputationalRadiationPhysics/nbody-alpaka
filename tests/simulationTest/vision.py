6aec3b8c7afb4188e9f60048f7f422f4b405a4be
Valentin Gehrke <madmaurice@zom.bi>
2016-01-25 19:07:52 +0100

    Refactored vision script

--- tests/simulationTest/vision.py
+++ tests/simulationTest/vision.py
@@ -1,8 +1,48 @@
 import numpy as np
 from visual import*
 
-def main():
+def main(filename, framerate):
     #get datas
+    with open(filename) as f:
+        datas = f.read()
+
+    #Parse Datas
+    lines = datas.split('\n')
+    masses=[(float(mass)) for mass in lines[0].split(' ')]
+
+    #Initialization
+    steps = []
+    for line in lines[1:]:
+        if len(line.strip()) < 3:
+            continue
+        bodies = []
+        for bodyinfo in line.split('|'):
+            point = tuple(map(float,bodyinfo.split(';')))
+            bodies.append(point)
+        steps.append(bodies)
+
+    v_points=[]
+    for i,position in enumerate(steps[0]):
+        v_points.append( sphere(pos=position,radius= 4 * masses[i]**0.3, color = color.white,  make_trail=True, material=materials.diffuse, retain=400))
+
+    #Update positions
+    while True :
+        for step in steps:
+            if len(step) != len(v_points):
+                print "Invalid step found"
+                break
+            rate(framerate)
+            newcenter = [ 0,0,0 ]
+            for i, position in enumerate(step):
+                for j in range(3):
+                    newcenter[j] += position[j]
+                v_points[i].pos = position
+            for j in range(3):
+                newcenter[j] /= len(step)
+
+            scene.center = tuple( newcenter )
+
+if __name__ == '__main__':
     from sys import argv
     filename = "test.txt"
     framerate = 50
@@ -10,36 +50,5 @@ def main():
         filename = argv[1]
     if len(argv) > 2:
         framerate = argv[2]
-    with open(filename) as f:
-        datas = f.read()
 
-    #Pharse Datas
-    datas= datas.split('\n')
-    masses=datas.pop(0).split(' ')
-    masses=[(float(mass))**0.3 for mass in masses]
-    datas.pop()
-
-    #initialisierung
-    points=datas.pop(0).split('|')
-    points= [tuple(map(float,p.split(';'))) for p in points]
-    v_points=[]
-    for i in range(len(points)):
-        v_points.append( sphere(pos=points[i],radius= 4 * masses[i], color = color.white,  make_trail=True, material=materials.diffuse, retain=400))
-
-    #update Positions
-    while True :
-        for data in datas:
-            rate(framerate)
-            points=data.split('|')
-            points=[tuple(map(float,p.split(';'))) for p in points]
-            newcenter = [ 0,0,0 ]
-            for i in range(len(v_points)):
-                for j in [0,1,2]:
-                    newcenter[j] += points[i][j]
-                v_points[i].pos= points[i]
-            for j in [0,1,2]:
-                newcenter[j] /= len(v_points)
-
-            scene.center = tuple( newcenter )
-
-main()
+    main(filename, framerate)
