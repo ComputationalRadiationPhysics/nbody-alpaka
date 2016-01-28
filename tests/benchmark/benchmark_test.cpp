e351138539116a0bb374730cd6c891cd028cee0d
Valentin Gehrke <madmaurice@zom.bi>
2016-01-29 00:27:20 +0100

    Fixed measuring issue. Using real time instead of cpu time now.

--- tests/benchmark/benchmark_test.cpp
+++ tests/benchmark/benchmark_test.cpp
@@ -1,14 +1,12 @@
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE SimulationClassTest
+//#define BOOST_TEST_DYN_LINK
+//#define BOOST_TEST_MODULE SimulationClassTest
 #include <iostream> // std::cout, std::endl;
 #include <simulation/types/vector.hpp> //Vector
 #include <simulation/simulation.hpp> // Simulation
-#include <boost/test/unit_test.hpp>
-#include <boost/mpl/list.hpp>
+//#include <boost/test/unit_test.hpp>
 #include <boost/type_index.hpp>
-#include <ctime>
-
-#define GET_CLOCK() (clock()/(float)CLOCKS_PER_SEC)
+#include <boost/timer.hpp>
+#include <chrono>
 
 using namespace nbody::simulation;
 
@@ -49,28 +47,29 @@ void runTest(std::size_t const NSize, std::size_t const NSteps)
                 smoothnessFactor,
                 gravitationalConstant);
 
-    float tStart, tEnd;
-    tStart = GET_CLOCK();
+
+    std::chrono::high_resolution_clock::time_point start =
+        std::chrono::high_resolution_clock::now();
     for(std::size_t i = 0; i < NSteps; i++) {
         sim.step(0.1f);
     }
-    tEnd = GET_CLOCK();
-
-    std::cout << "Time: " << (tEnd - tStart) << " secs" << std::endl;
+    std::chrono::high_resolution_clock::time_point end =
+        std::chrono::high_resolution_clock::now();
+    std::cout << "Time: " << 
+        std::chrono::duration_cast<std::chrono::milliseconds>
+            (end - start).count() / 1000.0f << " secs" << std::endl;
 
     delete[] bodiesPosition;
     delete[] bodiesVelocity;
     delete[] bodiesMass;
 }
 
-BOOST_AUTO_TEST_CASE( benchmark )
-{
-    for(int i=1 ; i <=32 ;i*=2)
-    {
-        runTest< 2,   float>(  i*128, 100 );
+int main(void) {
+    for(std::size_t i = 128; i <= 2048; i*=2) {
+        runTest<2,float>(i,1000);
     }
-    for(int i=1 ; i <=32 ;i*=2)
-    {
-        runTest< 3,   float>(  i*128, 100 );
+
+    for(std::size_t i = 128; i <= 2048; i*=2) {
+        runTest<3,float>(i,1000);
     }
 }
